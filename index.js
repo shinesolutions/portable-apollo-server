1b2b7904703c72231cd449685fb45c1a67f3c1c8
Ben Teese <ben.teese@shinesolutions.com>
2020-07-16 17:43:44 +1000

    Simplified

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const { ApolloServer } = require("apollo-server");
 
-const typeDefs = `
-  type Query {
-    helloWorld: String!
-  }
-`;
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    helloWorld: () => "Hello World!",
+const server = new ApolloServer({
+  typeDefs: `
+    type Query {
+      helloWorld: String!
+    }
+  `,
+  resolvers: {
+    Query: {
+      helloWorld: () => "Hello World!",
+    },
   },
-};
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
 });
+
+server.listen().then(() => console.log("Ready!"));
