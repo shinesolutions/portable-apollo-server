af9498b6e69c181601c5e0bad742bfe162d734aa
Ben Teese <ben.teese@shinesolutions.com>
2020-07-17 17:49:39 +1000

    Got stubby running with tests

--- src/test.js
+++ src/test.js
@@ -1,11 +1,35 @@
 const { ApolloServer } = require("apollo-server");
 const { request } = require("graphql-request");
-const { config } = require("./config");
+const { createConfig } = require("./config");
+const { Stubby } = require("stubby");
+const { promisify } = require("util");
+const yaml = require("js-yaml");
+const fs = require("fs");
+const path = require("path");
 
 let serverInfo;
+let stubby;
 
 beforeAll(async () => {
-  const apolloServer = new ApolloServer(config);
+  stubby = new Stubby();
+
+  // Start all stubby services on ephemeral ports
+  await promisify(stubby.start).bind(stubby)({
+    stubs: 0,
+    admin: 0,
+    tls: 0,
+    data: yaml.safeLoad(
+      fs.readFileSync(path.join(__dirname, "..", "stubby.yaml"))
+    ),
+  });
+  const stubsAddress = stubby.stubsPortal.address();
+
+  const apolloServer = new ApolloServer(
+    createConfig({
+      helloWorldUrl: `http://${stubsAddress.address}:${stubsAddress.port}`,
+    })
+  );
+
   // Start server on ephemeral port
   serverInfo = await apolloServer.listen({ port: 0 });
 });
@@ -20,6 +44,8 @@ test("epoch", async () => {
   expect(data.epoch).toEqual("1970-01-01T00:00:00.000Z");
 });
 
-afterAll((done) => {
-  serverInfo.server.close(done);
+afterAll(async () => {
+  await promisify(stubby.stop).bind(stubby)();
+  const { server } = serverInfo;
+  await promisify(server.close).bind(server)();
 });
