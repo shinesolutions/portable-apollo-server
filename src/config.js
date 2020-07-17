0e1155b4987f058f06200f12c5eb371a754db645
Ben Teese <ben.teese@shinesolutions.com>
2020-07-17 15:28:39 +1000

    Added scalar type

--- src/config.js
+++ src/config.js
@@ -1,12 +1,19 @@
+const { GraphQLDateTime } = require("graphql-iso-date");
+
 exports.config = {
   typeDefs: `
+    scalar DateTime
+
     type Query {
       helloWorld: String!
+      epoch: DateTime!
     }
   `,
   resolvers: {
+    DateTime: GraphQLDateTime,
     Query: {
       helloWorld: () => "Hello World!",
+      epoch: () => new Date(0),
     },
   },
 };
